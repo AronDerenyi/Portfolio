5ffaaea81fddaddb8594b5a4eba25a5b2be54369
Aron Derenyi <aron.derenyi@gmail.com>
2022-10-24 12:51:21 +0200

    WIP artoolkit and threejs

--- artoolkit/artoolkit.ts
+++ artoolkit/artoolkit.ts
@@ -1,29 +1,69 @@
 export class ARToolkit {
 
-    private worker: Worker
-    private video: HTMLVideoElement
-    private canvas: HTMLCanvasElement
-    private context: CanvasRenderingContext2D
+    private readonly worker: Worker
+    private readonly video: HTMLVideoElement
+    private readonly canvas: HTMLCanvasElement
+    private readonly context: CanvasRenderingContext2D
 
-    constructor(private onUpdate: (ImageData) => void) {
+    constructor(private onUpdate: (image: ImageData, projection: Float64Array, transform: Float64Array) => void) {
         this.worker = new Worker(
             new URL('artoolkit.worker.ts', import.meta.url),
             {type: 'module'}
         )
-        this.worker.postMessage('init')
+        this.worker.onmessage = event => this.onMessage(event.data)
 
         this.video = document.createElement('video')
         this.canvas = document.createElement('canvas')
         this.context = this.canvas.getContext('2d')
-        this.update()
+
+        navigator.mediaDevices
+            .getUserMedia({
+                audio: false,
+                video: {facingMode: "environment"}
+            })
+            .then(stream => {
+                const settings = stream.getVideoTracks()[0].getSettings();
+                this.canvas.width = settings.width || 0;
+                this.canvas.height = settings.height || 0;
+                this.video.srcObject = stream;
+                this.worker.postMessage({
+                    type: 'init',
+                    width: settings.width,
+                    height: settings.height,
+                    camera: '/camera.dat',
+                    marker: '/marker/pinball'
+                })
+
+                return this.video.play()
+            })
+            .catch(error => {
+                console.log("An error occured! " + error);
+            })
     }
 
     destroy() {
         this.worker.terminate()
     }
 
-    private update() {
-        this.onUpdate(null)
-        requestAnimationFrame(() => this.update())
+    private onMessage(msg: any) {
+        if (msg.type === 'update') {
+            this.onUpdate(msg.image, msg.projection, msg.transform)
+        }
+
+        if (msg.type === 'ready' || msg.type === 'update') {
+            this.worker.postMessage({
+                type: 'process',
+                normalImage: this.getVideoFrame(this.canvas.width, this.canvas.height),
+                smallImage: this.getVideoFrame(720, 720 * this.canvas.height / this.canvas.width)
+            })
+        }
+    }
+
+    private getVideoFrame(width: number, height: number): ImageData {
+        width = Math.min(width, this.canvas.width)
+        height = Math.min(height, this.canvas.height)
+
+        this.context.drawImage(this.video, 0, 0, width, height)
+        return this.context.getImageData(0, 0, width, height)
     }
 }
--- artoolkit/artoolkit.worker.ts
+++ artoolkit/artoolkit.worker.ts
@@ -1,12 +1,60 @@
+import ARToolkitNFT from '@webarkit/jsartoolkit-nft'
+
 self.onmessage = event => {
     const msg = event.data
-    console.log(msg)
+    switch (msg.type) {
+        case 'init':
+            init(msg.width, msg.height, msg.camera, msg.marker)
+            break
+        case 'process':
+            process(msg.normalImage, msg.smallImage)
+            break
+    }
 }
 
-function init(msg) {
+let ar: any = null
+let transform: Float64Array = null
+let ready = false
 
+function init(width, height, camera, marker) {
+    ARToolkitNFT.ARControllerNFT
+        .initWithDimensions(
+            width,
+            height,
+            camera
+        )
+        .then(arController => {
+            ar = arController
+
+            ar.addEventListener("getNFTMarker", event => {
+                transform = event.data.matrixGL_RH
+            });
+
+            ar.loadNFTMarker(marker,
+                id => {
+                    ar.trackNFTMarkerId(id);
+                    postMessage({type: "ready"});
+                    ready = true
+                },
+                () => {
+                    console.log("An error occured while loading NFT marker");
+                }
+            ).then()
+        })
+        .catch(error => {
+            console.log("An error occured! " + error);
+        })
 }
 
-function test(msg) {
+function process(normalImage: ImageData, smallImage: ImageData) {
+    if (!ready) return
 
+    transform = null
+    ar.process(smallImage);
+    postMessage({
+        type: "update",
+        image: normalImage,
+        projection: ar.getCameraMatrix(),
+        transform
+    });
 }
